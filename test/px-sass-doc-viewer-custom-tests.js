f1f16eef6652a30ba3ef6a7af2e94485a95d8504
Randy Askin <randall.askin@ge.com>
2016-09-13 15:21:45 -0700

    Initial commit

---
+++ test/px-sass-doc-viewer-custom-tests.js
@@ -0,0 +1,27 @@
+// This is the wrapper for custom tests, called upon web components ready state
+function runCustomTests() {
+  // Place any setup steps like variable declaration and initialization here
+
+  // This is the placeholder suite to place custom tests in
+  // Use testCase(options) for a more convenient setup of the test cases
+  suite('Custom Automation Tests for px-sass-doc-viewer', function() {
+    test('Check initial value of counter', function(done){
+      var counterEl = Polymer.dom(document).querySelector('px-sass-doc-viewer'),
+          counterValueEl = Polymer.dom(counterEl.root).querySelector('span');
+      assert.equal(counterValueEl.textContent, '0');
+      done();
+    });
+
+    test('Clicking px-sass-doc-viewer increments the counter', function(done){
+      var counterEl = Polymer.dom(document).querySelector('px-sass-doc-viewer'),
+          counterValueEl = Polymer.dom(counterEl.root).querySelector('span');
+      assert.equal(counterValueEl.textContent, '0');
+
+      counterEl.click();
+      flush(function(){
+        assert.equal(counterValueEl.textContent, '1');
+      });
+      done();
+    });
+  });
+};
